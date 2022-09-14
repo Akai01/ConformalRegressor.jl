4fdcf01d73f46cce40e39f14526bf7b2a1dbc0bb
Resul Akay <resulakay1@gmail.com>
2022-09-14 13:15:58 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/ConformalRegressor.jl
@@ -0,0 +1,5 @@
+module ConformalRegressor
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ConformalRegressor
+using Test
+
+@testset "ConformalRegressor.jl" begin
+    # Write your tests here.
+end
