c940986600fc57b2e0c790f551701a1e8df70df1
Resul Akay <resulakay1@gmail.com>
2022-09-14 13:29:01 +0200

    initial commit

--- src/ConformalRegressor.jl
+++ src/ConformalRegressor.jl
@@ -1,5 +1,25 @@
 module ConformalRegressor
 
-# Write your package code here.
+struct ConformalRegressor
+    normalized::Bool
+    alphas::Vector
+end
+
+function Fit(residuals::Vector, sigmas::Union{Vector, Nothing} = nothing)
+    abs_residuals = abs.(residuals)
+    if isnothing(sigmas)
+       normalized = false
+       alphas = reverse(sort!(abs_residuals))
+    else 
+        normalized = true
+        alphas = reverse(sort!(abs_residuals ./ sigmas))
+    end
+    return ConformalRegressor(normalized, alphas)
+end
+
+
+function predict(object::ConformalRegressor, y_hat::Vector, sigmas::Vector, confidence::Vector, y_min::Union{Int, float}, y_max::Union{Int, float})
+    
+end
 
 end
